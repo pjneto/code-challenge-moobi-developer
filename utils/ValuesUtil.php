be8dd7f8111312a01cf3ba81431eb9e64f45ca38
ishynoris <ishynoris@github.com>
2019-06-13 19:09:17 -0300

    Formatting Date

--- utils/ValuesUtil.php
+++ utils/ValuesUtil.php
@@ -9,4 +9,18 @@ class ValuesUtil {
     public static function is_null_or_empty(string $value): bool {
         return is_null($value) || strlen($value) === 0;
     }
+
+    public static function format_date(string $value = null): string {
+        if (is_null($value)) {
+            $value = time();
+        }
+        return date('Y-m-d H:i:s', $value);
+    }
+
+    public static function show_date(string $date, bool $time = false): string {
+        $format = "d/m/Y";
+        $format .= boolval($time) ? " H:m:s" : "";
+        
+        return date($format, strtotime($date));
+    }
 }
