d539138a34227b14a1de60d9cea19804b46472c9
ishynoris <mota.a.santos@gmail.com>
2019-06-13 22:58:39 -0300

    Bug at get base url

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -26,7 +26,11 @@ abstract class Controller {
 
     public static function base_url(string $path): string {
         $size = sizeof($_GET);
-        return $size === 3 ? "../../"
-                : ($size === 2 ? "../" : "") . $path; 
+        $paths = [
+            1 => $path,
+            2 => "../$path",
+            3 => "../../$path",
+        ];
+        return $paths[$size] ?? $path;
     }
 }
