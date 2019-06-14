0d3a9c36d182efdf80b7a4196f537a9beb7354b6
ishynoris <mota.a.santos@gmail.com>
2019-06-13 23:37:30 -0300

    - Activate/Inactivate at Details View - Disable fileds when inactive

--- views/ProductDetailView.php
+++ views/ProductDetailView.php
@@ -5,6 +5,9 @@ $prodController = new ProductController;
 $result = $prodController->post($prodController->input_name());
 $id = isset($_GET['value']) ? intval($_GET['value']) : -1;
 $product = $prodController->get_product($id);
+
+$disabled = $product->codStatus === PRO_INACTIVE ? "disabled" : "";
+$btnStatus = $product->codStatus === PRO_INACTIVE ? "Activate" : "Inactivate";
 ?>
 
 <title>Product Details</title>
@@ -16,23 +19,24 @@ $product = $prodController->get_product($id);
     <form method="post">
         <div class="form-field">
             <label for="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Product name" value="<?= $product->name ?>">
+            <input type="text" id="name" name="name" placeholder="Product name" value="<?= $product->name ?>" <?= $disabled ?>>
         </div>
         <div class="form-field">
             <label for="barcode">Barcode</label>
-            <input id="barcode" name="barcode" placeholder="Barcode" value="<?= $product->barcode ?>">
+            <input id="barcode" name="barcode" placeholder="Barcode" value="<?= $product->barcode ?>" <?= $disabled ?>>
         </div>
         <div class="form-field">
             <label for="price">Price</label>
-            <input type="number" id="price" name="price" placeholder="Unit price" value="<?= $product->price ?>">
+            <input type="number" id="price" name="price" placeholder="Unit price" value="<?= $product->price ?>" <?= $disabled ?> >
         </div>
         <div class="form-field">
             <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="4" placeholder="Some product's description"><?= $product->description?></textarea>
+            <textarea name="description" id="description" cols="30" rows="4" placeholder="Some product's description" <?= $disabled ?>><?= $product->description?></textarea>
         </div>
         <div class="form-field">
             <label></label>
-            <button type="submit" name="btn-edit" value="<?= $product->id ?>">Edit</button>
+            <button type="submit" name="btn-inactivate" value="<?= $product->id ?>"><?= $btnStatus ?></button> 
+            <button type="submit" name="btn-edit" value="<?= $product->id ?>" <?= $disabled ?>>Edit</button>
         </div>
     </form>
 
