f32c841b5421132febeee80c64f7275a2704e347
ishynoris <mota.a.santos@gmail.com>
2019-06-19 02:54:32 -0300

    - Dev Environment - Potencial bug

--- configs.php
+++ configs.php
@@ -1,6 +1,12 @@
 <?php 
+define("ENVIRONMENT", "DEV"); 
+// define("ENVIRONMENT", "HOMOL"); 
+// define("ENVIRONMENT", "PROD"); 
 
-define("DB_NAME", "bd_toy_shop");
+define("DEV_DB_NAME", "dev_bd_toy_shop");
+define("HOMOL_DB_NAME", "homol_bd_toy_shop");
+define("PROD_DB_NAME", "bd_toy_shop");
+
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
--- src/utils/ValuesUtil.php
+++ src/utils/ValuesUtil.php
@@ -2,11 +2,11 @@
 
 class ValuesUtil {
 
-    public static function value_or_default(array $values, string $key, $default = null): string {
+    public static function value_or_default(array $values, string $key, $default = null): ?string {
         return isset($values[$key]) ? $values[$key] : $default;
     }
 
-    public static function is_null_or_empty(string $value): bool {
+    public static function is_null_or_empty(?string $value): bool {
         return is_null($value) || strlen($value) === 0;
     }
 
