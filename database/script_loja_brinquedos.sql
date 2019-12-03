2c7108e9f4cf8e87b2ffa48c7e792b6fa8b09fec
Antonio Martins <carlos12antoni@gmail.com>
2019-12-02 21:01:27 -0300

    (database) script database

---
+++ database/script_loja_brinquedos.sql
@@ -0,0 +1,101 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 100408
+ Source Host           : localhost:3306
+ Source Schema         : loja-brinquedos
+
+ Target Server Type    : MySQL
+ Target Server Version : 100408
+ File Encoding         : 65001
+
+ Date: 30/11/2019 22:27:48
+*/
+
+CREATE DATABASE IF NOT EXISTS loja_brinquedos;
+USE loja_brinquedos;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for clientes
+-- ----------------------------
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE `clientes`  (
+  `idcliente` int(10) NOT NULL AUTO_INCREMENT,
+  `dataCad` datetime(0) NOT NULL,
+  `ativo` enum('S','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'S',
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `sexo` enum('F','M') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'M',
+  `dataNasc` date NULL DEFAULT NULL,
+  `tipoDoc` enum('CPF','CNPJ') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `documento` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `endereco` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `cep` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `logradouro` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `bairro` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `numero` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `complemento` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `estado` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `cidade` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `telefone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `celular` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idcliente`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pedidos
+-- ----------------------------
+DROP TABLE IF EXISTS `pedidos`;
+CREATE TABLE `pedidos`  (
+  `idPedido` int(10) NOT NULL,
+  `dataCad` datetime(0) NOT NULL,
+  `ativo` enum('S','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'S',
+  `dataPedido` date NOT NULL,
+  `formaPagamento` enum('CD','DE','BO') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `numParcelas` int(10) NULL DEFAULT NULL,
+  `idCliente` int(10) NOT NULL,
+  `valorTotalVenda` decimal(12, 2) NOT NULL,
+  `valorParcela` decimal(12, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`idPedido`) USING BTREE,
+  INDEX `fk_pedido_cliente`(`idCliente`) USING BTREE,
+  CONSTRAINT `fk_pedido_cliente` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idcliente`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pedidos_produtos
+-- ----------------------------
+DROP TABLE IF EXISTS `pedidos_produtos`;
+CREATE TABLE `pedidos_produtos`  (
+  `idPedidoProduto` int(10) NOT NULL AUTO_INCREMENT,
+  `idProduto` int(10) NULL DEFAULT NULL,
+  `idPedido` int(10) NULL DEFAULT NULL,
+  `dataCad` datetime(0) NULL DEFAULT NULL,
+  `ativo` enum('S','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'S',
+  `quantidade` int(10) NOT NULL DEFAULT NULL,
+  PRIMARY KEY (`idPedidoProduto`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for produtos
+-- ----------------------------
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE `produtos`  (
+  `idProduto` int(10) NOT NULL AUTO_INCREMENT,
+  `dataCad` datetime(0) NOT NULL,
+  `ativo` enum('S','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'S',
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `modelo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `marca` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `preco` decimal(12, 2) NOT NULL,
+  `observacoes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `quantidade` int(10) NOT NULL,
+  PRIMARY KEY (`idProduto`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
