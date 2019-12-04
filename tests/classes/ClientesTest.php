0d9c59c001b96901474259c1cedb725814699a30
Antonio Martins <carlos12antoni@gmail.com>
2019-12-04 02:19:40 -0300

    (test) Configurando phpunit e iniciando arquivos de teste

---
+++ tests/classes/ClientesTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace CodeChallengeMoobi;
+
+class ClientesTest extends \PHPUnit_Framework_TestCase
+{
+	public function testeRetornarClientes() 
+	{
+		$cliente = new Clientes();
+	}
+}
