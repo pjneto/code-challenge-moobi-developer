20d07f7101f83f47f3cb255d847db8c7b05e62c6
Antonio Martins <carlos12antoni@gmail.com>
2019-12-03 23:30:32 -0300

    (feat) nova classe de itens/produtos de um pedido

---
+++ app/classes/PedidosProdutos.php
@@ -0,0 +1,68 @@
+<?php
+namespace CodeChallengeMoobi;
+
+use CodeChallengeMoobi\Conexao;
+
+class PedidosProdutos
+{
+    protected $tabela;
+    protected $objConexao;
+
+    public function __construct()
+    {
+        $this->objConexao = new Conexao\AcessoBanco();
+        $this->tabela = 'pedidos_produtos';
+        $this->chave = 'idPedidoProduto';
+        $this->chaveProduto = 'idProduto';
+        $this->chavePedido = 'idPedido';
+    }
+
+    public function retornarPedidosProdutos()
+    {
+        return $this->objConexao->select($this->tabela);
+    }
+
+    public function consultarProdutosDeUmPedido($idPedido)
+    {
+        return $this->objConexao->selectById($this->tabela, $idPedido, $this->chavePedido);
+    }
+
+    public function cadastrarPedidosProdutos(array $produtos, $idPedido)
+    {
+        try {
+           
+           foreach ($produtos as $idProduto) {
+                $dados = $this->montarDados($idProduto, $idPedido);
+                $this->objConexao->insert($this->tabela, $dados);
+           }
+
+           return true;
+
+        } catch(\Exception $erro) {
+            echo $erro->getMessage();
+            exit;
+        }
+    }
+
+    public function montarDados($idProduto, $idPedido)
+    {
+        $dados['idProduto'] = $idProduto;
+        $dados['idPedido'] = $idPedido;
+        $dados['ativo'] = 'S';
+        $dados['dataCad'] = date('Y-m-d H:i:s');
+
+        return $dados;
+    }
+
+    public function inativarPedidoProduto($idPedidoProduto)
+    {
+        $dados['ativo'] = 'N';
+        return $this->objConexao->update($this->tabela, $dados, $idPedidoProduto, $this->chave);
+    }
+
+    public function apagarPedidoProduto($idPedidoProduto)
+    {
+    	return $this->objConexao->delete($this->tabela, $idPedidoProduto, $this->chave);
+    }
+
+}
