eb1d45fc4189faaac9414c402f6cf6ded117a211
Antonio Martins <carlos12antoni@gmail.com>
2019-12-05 01:51:57 -0300

    (feat) classe para testes de clientes

--- tests/classes/ClientesTest.php
+++ tests/classes/ClientesTest.php
@@ -1,10 +1,81 @@
 <?php
-namespace CodeChallengeMoobi;
+require 'vendor/autoload.php';
+require 'config/config.php';
 
-class ClientesTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class ClientesTest extends TestCase
 {
+
+	public function getObjetoCliente()
+	{
+		return new CodeChallengeMoobi\Clientes();
+	}
+
 	public function testeRetornarClientes() 
 	{
-		$cliente = new Clientes();
+		$objCliente = $this->getObjetoCliente();
+		$qtdeClientes = count($objCliente->retornarClientes());
+		$this->assertEquals(3, $qtdeClientes);
 	}
+
+	public function testeConsultarCliente()
+	{
+		$objCliente = $this->getObjetoCliente();
+		$cliente = $objCliente->consultarCliente(100);
+		$this->assertEquals('Antonio', $cliente[0]->nome);
+	}
+
+	public function testeCadastrarClienteSucesso()
+	{
+		$dadosCliente = [
+			'dataCad' 	=> date('Y-m-d H:i:s'),
+			'ativo' 	=> 'S',
+			'nome' 		=> 'Maria',
+			'sexo' 		=> 'F',
+			'dataNasc' 	=> '1995-10-15',
+			'tipoDoc' 	=> 'CPF',
+			'documento' => '01234567899',
+			'telefone' 	=> '79999874563',
+			'celular' 	=> '79454547785',
+			'email' 	=> 'maria@email.com'
+		];
+
+		$objCliente = $this->getObjetoCliente();
+		$resultadoCadastro = $objCliente->cadastrarCliente($dadosCliente);
+		$this->assertEquals(true, $resultadoCadastro);
+	}
+
+	public function testeCadastrarClienteErro()
+	{
+		$dadosCliente = [
+			'dataCad' 	=> date('Y-m-d H:i:s'),
+			'ativo' 	=> 'S',
+			'nome' 		=> 'João',
+			'tipoDoc' 	=> 'CPF',
+			'documento' => '01234567890',
+			'telefone' 	=> '79999874563',
+			'email'		=> 'email@email.com'
+		];
+
+		$objCliente = $this->getObjetoCliente();
+		$resultadoCadastro = $objCliente->cadastrarCliente($dadosCliente);
+		$this->assertEquals(CELULAR_NAO_INFORMADO, $resultadoCadastro);
+	}
+
+	public function testeAtualizarCliente()
+	{
+		$objCliente = $this->getObjetoCliente();
+		$novosDados = ['email' => 'lojaonline@loja.com'];
+		$resultUpdate = $objCliente->atualizarCliente($novosDados, 102);
+		$this->assertEquals(true, $resultUpdate);
+	}
+
+	public function testeInativarCliente()
+	{
+		$objCliente = $this->getObjetoCliente();
+		$resultUpdate = $objCliente->inativarCliente(102);
+		$this->assertEquals(true, $resultUpdate);
+	}
+
 }
